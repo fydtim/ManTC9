9ccb4e121de13f8a7713ea3a4ee6f90b9e2e0e3d
Tim McHugh <tim@freeyourdata.net>
2012-05-11 22:26:51 -0400

    first commit of template code

---
+++ src/net/freeyourdata/android/apps/mantc9/poc/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package net.freeyourdata.android.apps.mantc9.poc.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
